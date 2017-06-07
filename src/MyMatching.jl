4f7a970bb81e2440459ed50dc643f3f9975fccf1
NTsuchiya0127 <angelbeatskanade61st@yahoo.co.jp>
2017-06-07 15:03:02 +0900

    Update MyMatching.jl

--- src/MyMatching.jl
+++ src/MyMatching.jl
@@ -1,5 +1,5 @@
 module MyMatching
-function my_deferred_acceptance(m_prefs, f_prefs)
+function MyMatching(m_prefs, f_prefs)
     m = length(m_prefs)
     n = length(f_prefs)
     m_matched = Array{Int64}(1, m)
@@ -7,36 +7,36 @@ function my_deferred_acceptance(m_prefs, f_prefs)
     m_matched[1:end] = 0
     f_matched[1:end] = 0
     h = 1
-    while h <= n 
+    while h <= n
         for j in 1:m
             k = 1
-            while k <= length(m_prefs[j]) && m_matched[j] == 0
-                if f_matched[m_prefs[j][k]] == 0
-                    if sum(f_prefs[m_prefs[j][k]] .== j) == 1
-                        m_matched[j] = m_prefs[j][k]
-                        f_matched[m_prefs[j][k]] = j
+            while k <= length(m_prefs[j])
+                if m_matched[j] == 0
+                    if f_matched[m_prefs[j][k]] == 0
+                        if sum(f_prefs[m_prefs[j][k]] .== j) == 1
+                            m_matched[j] = m_prefs[j][k]
+                            f_matched[m_prefs[j][k]] = j
+                        end
                     else
-                        f_matched[m_prefs[j][k]] = 0
-                    end
-                else
-                    p = f_matched[m_prefs[j][k]]
-                    q = 1
-                    r = 1
-                    while !(f_prefs[m_prefs[j][k]][q] == j)
-                        q += 1
-                    end
-                    while !(f_prefs[m_prefs[j][k]][r] == p)
-                        r += 1
-                    end
-                    if q < r
-                        m_matched[p] = 0
-                        m_matched[j] = m_prefs[j][k]
-                        f_matched[[m_prefs[j][k]]] = j
+                        p = f_matched[m_prefs[j][k]]
+                        q = 1
+                        r = 1
+                        while !(f_prefs[m_prefs[j][k]][q] == j)
+                            q += 1
+                        end
+                        while !(f_prefs[m_prefs[j][k]][r] == p)
+                            r += 1
+                        end
+                        if q < r
+                            m_matched[p] = 0
+                            m_matched[j] = m_prefs[j][k]
+                            f_matched[[m_prefs[j][k]]] = j
+                        end
                     end
                 end
                 k += 1
             end
-        end    
+        end 
         h += 1
     end
     return m_matched, f_matched
