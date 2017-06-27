b66e104a9e37182e72fba177aba477874b1f4b7e
NTsuchiya0127 <angelbeatskanade61st@yahoo.co.jp>
2017-06-27 16:47:48 +0900

    Update MyMatching.jl

--- src/MyMatching.jl
+++ src/MyMatching.jl
@@ -63,7 +63,7 @@ end
 function my_deferred_acceptance(prop_prefs::Vector{Vector{Int}},
                     resp_prefs::Vector{Vector{Int}})
                     caps = ones(Int, length(resp_prefs))
-    prop_matched, resp_matched, indptr = MyMatching(prop_prefs, resp_prefs, caps)
+    prop_matched, resp_matched, indptr = my_deferred_acceptance(prop_prefs, resp_prefs, caps)
     return prop_matched, resp_matched
 end
 
